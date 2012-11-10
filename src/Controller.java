04cf921deb40481a343422131bda2a7536f6865e
Chris Boyce <christopher.d.boyce@gmail.com>
2012-11-10 14:24:00 -0800

    saving before refactor to raw group

--- src/Controller.java
+++ src/Controller.java
@@ -6,16 +6,27 @@ import org.stringtemplate.v4.STGroupDir;
 public class Controller {
 
 	private String className;
+	private String controllerType;
 	
 	public Controller(String name){
 		className = name;
+		controllerType = Util.CreateTypeFromClassName(name);
+	}
+	
+	public String getClassName(){
+		return className;
+	}
+	public String getControllerType(){
+		return controllerType;
 	}
 	public static void main(String[] args) {
-		String controllerName = "CoupleArea";
+		
 		STGroup group = new STGroupDir("templates");
 		ST st = group.getInstanceOf("controller");
-		st.add("controllerName",controllerName);
-		st.add("controllerType", Util.CreateTypeFromClassName(controllerName));
+		st.add("controller",new Controller("CoupleArea"));
+		
+		//st.add("controllerName",controllerName);
+		//st.add("controllerType", Util.CreateTypeFromClassName(controllerName));
 		String result = st.render();
 		//st.inspect();
 		System.out.println(result);
